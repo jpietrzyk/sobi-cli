2f3e99f7e1be0bcd6316a6b5916c04b6fa6b356a
Jacek Pietrzyk <jacpie3k@gmail.com>
2017-01-21 14:34:02 +0100

    Fix problem with no-integer bike_id

--- lib/sobi_cli/clients/bike_client.rb
+++ lib/sobi_cli/clients/bike_client.rb
@@ -7,7 +7,7 @@ module SobiCli
       include SobiCli::Helpers::BikeHelper
 
       def initialize(bike_id)
-        $redis.set short_id , {bike_id: bike_id, token: token}.to_json
+        $redis.set short_id , {bike_id: bike_id.to_i, token: token}.to_json
       end
     end
   end
